f4be53adbd06d1760ad0c690281287d96e555645
Sravy <svelaga0412@gmail.com>
2018-02-23 18:01:20 -0800

    final commit

---
+++ WebApplication2/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(ChatMessage message)
+        {
+            Clients.All.GetMessage(message);
+        }
+
+        public class ChatMessage
+        {
+            public string Name { get; set; }
+            public string MessageStr { get; set; }
+        }
+            
+
+    }
+}
